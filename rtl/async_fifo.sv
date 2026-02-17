c4c5710218fe0778c6107a7db12d641372eba6f2
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-02-17 17:45:02 -0300

    Update async_fifo.sv

--- rtl/async_fifo.sv
+++ rtl/async_fifo.sv
@@ -5,19 +5,19 @@ module AsyncFifo
   )
   (
     // Write Domain
-    input  logic wr_clk,                   // Write clock
-    input  logic wr_rst_n,                 // Active-low write reset (async or sync — see notes)
-    input  logic wr_en,                    // Write request (one entry per cycle when accepted)
+    input  logic wr_clk,             // Write clock
+    input  logic wr_rst_n,           // Active-low write reset (async or sync — see notes)
+    input  logic wr_en,              // Write request (one entry per cycle when accepted)
     input  logic [BITS-1:0] wr_data, // Data to write
-    output logic wr_full,                  // FIFO full flag (do not write when 1)
+    output logic wr_full,            // FIFO full flag (do not write when 1)
     // output logic wr_almost_full,          // (Optional) Programmable threshold
     // output logic wr_level,                // (Optional) Approximate fill level (write domain view)
     // Read Domain
-    input  logic rd_clk,                   // Read clock
-    input  logic rd_rst_n,                 // Active-low read reset
-    input  logic rd_en,                    // Read request (one entry per cycle when accepted)
+    input  logic rd_clk,             // Read clock
+    input  logic rd_rst_n,           // Active-low read reset
+    input  logic rd_en,              // Read request (one entry per cycle when accepted)
     output logic [BITS-1:0] rd_data, // Data read
-    output logic rd_empty                  // FIFO empty flag (do not read when 1)
+    output logic rd_empty            // FIFO empty flag (do not read when 1)
     // output logic rd_almost_empty,         // (Optional) Programmable threshold
     // output logic rd_level,                // (Optional) Approximate fill level (read domain view)
     );
