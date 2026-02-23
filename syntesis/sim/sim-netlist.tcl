a9b178620412f284c3d9b6c0afc0ea843676572b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-02-23 01:03:33 -0300

    Update sim-netlist.tcl

--- syntesis/sim/sim-netlist.tcl
+++ syntesis/sim/sim-netlist.tcl
@@ -3,9 +3,9 @@ vlib work
 vmap work work
 
 
-vlog -work work $define_flags -svinputport=relaxed /pdk/tsmc/PDK28/PDK_TSMC28_bv/tcbn28hpcplusbwp30p140_190a/TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp30p140_110a/tcbn28hpcplusbwp30p140.v
-vlog -work work $define_flags -svinputport=relaxed ../logical/results/gate_level/async_fifo_logic_mapped.v
-vlog -work work $define_flags -svinputport=relaxed ../../tb/test_async_fifo.sv
+vlog -work work -svinputport=relaxed /pdk/tsmc/PDK28/PDK_TSMC28_bv/tcbn28hpcplusbwp30p140_190a/TSMCHOME/digital/Front_End/verilog/tcbn28hpcplusbwp30p140_110a/tcbn28hpcplusbwp30p140.v
+vlog -work work -svinputport=relaxed ../logical/results/gate_level/async_fifo_logic_mapped.v
+vlog -work work -svinputport=relaxed ../../tb/test_async_fifo.sv
 # to show FSM
 # vsim -voptargs=+acc -t ns -fsmdebug -coverage -debugDB work.tb
 vsim -voptargs=+acc -t ns work.tb
