e1f8ac6bd758a2964581daba8c584065d00d407b
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-02-23 01:30:14 -0300

    Add power estimation and report output
    Read switching stimulus when available and run Joules-based power
    evaluation, producing power_evaluation.txt. Update Genus and xrun logs,
    timestamps, and minor script timing info.

--- syntesis/power/genus.cmd
+++ syntesis/power/genus.cmd
@@ -1,6 +1,6 @@
 # Cadence Genus(TM) Synthesis Solution, Version 23.14-s090_1, built Feb 27 2025 10:49:50
 
-# Date: Mon Feb 23 01:14:53 2026
+# Date: Mon Feb 23 01:28:59 2026
 # Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.83.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
 # OS:   Rocky Linux 8.10 (Green Obsidian)
 
