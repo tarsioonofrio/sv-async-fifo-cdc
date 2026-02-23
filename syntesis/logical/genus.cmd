1c50ce8284381d1b39f0fd4924537a66e059d8b3
Társio Onofrio <5504075+tarsioonofrio@users.noreply.github.com>
2026-02-23 13:56:51 -0300

    Add new synth data

--- syntesis/logical/genus.cmd
+++ syntesis/logical/genus.cmd
@@ -1,6 +1,6 @@
 # Cadence Genus(TM) Synthesis Solution, Version 21.12-s068_1, built Mar  4 2022 14:12:46
 
-# Date: Sun Feb 22 23:28:28 2026
+# Date: Mon Feb 23 13:45:21 2026
 # Host: paxos.inf.pucrs.br (x86_64 w/Linux 4.18.0-553.83.1.el8_10.x86_64) (14cores*28cpus*1physical cpu*Intel(R) Core(TM) i9-7940X CPU @ 3.10GHz 19712KB)
 # OS:   Rocky Linux release 8.10 (Green Obsidian)
 
